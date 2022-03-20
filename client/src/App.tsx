343573177e9495c4ff16b443f59cb276b742ec5d
elina <elinapark0818@gmail.com>
2022-03-20 21:17:23 +0900

    Fix: boilerplating 초기설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 // import axios from 'axios';
 
 // interface User {
@@ -6,17 +6,19 @@ import React from 'react';
 // 	firstName: string;
 // }
 
-const App: React.FC = () => {
-	// axios.get<User[]>('http://localhost:4000').then((res) => {
-	// 	console.log(res.data);
-	// });
-	return (
-		<>
-			<div>
-				<h1>아ㅏ 되고있구나 ㅠㅠ</h1>
-			</div>
-		</>
-	);
-};
+// ? 굳이 React.FC 안쓰고 함수선언식으로 해도 되니까 화살표함수를 안쓰는 방향으로 해보자!
+
+function App() {
+  // axios.get<User[]>('http://localhost:4000').then((res) => {
+  // 	console.log(res.data);
+  // });
+  return (
+    <>
+      <div>
+        <h1>아ㅏ 되고있구나 ㅠㅠ</h1>
+      </div>
+    </>
+  );
+}
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 // import { Provider } from "react-redux";
-import GlobalStyle from './styles/global-styles';
-import theme from './styles/theme';
-import { ThemeProvider } from './styles/themed-components';
 
 ReactDOM.render(
-	<>
-		<GlobalStyle />
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
-	</>,
-	document.getElementById('root'),
+  <>
+    <App />
+  </>,
+  document.getElementById("root")
 );
