34253ec02ea69f954b028bbb6e32f60878e973f3
biblePark <stzard@gmail.com>
2022-03-23 14:26:25 +0900

    환경변수 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/vting/server/dist
-
-export DATABASE_URL=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_URL --query Parameters[0].Value | sed 's/"//g')
-export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+cd /home/ubuntu/vting/server
 
 authbind --deep pm2 start index.js
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/vting/server/dist
+cd /home/ubuntu/vting/server
 pm2 stop index.js 2> /dev/null || true
 pm2 delete index.js 2> /dev/null || true
