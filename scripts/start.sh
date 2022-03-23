75f6763bb722ad89d6b6da07642af02bf9e55964
biblePark <stzard@gmail.com>
2022-03-23 14:34:35 +0900

    initail 경로 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/vting/server
+cd /home/ubuntu/vting/server/dist
 
 authbind --deep pm2 start index.js
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/vting/server
+cd /home/ubuntu/vting/server/dist
 pm2 stop index.js 2> /dev/null || true
 pm2 delete index.js 2> /dev/null || true
