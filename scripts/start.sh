942fc768d86940be9a02336d7936ef37484de223
biblePark <stzard@gmail.com>
2022-03-27 14:17:44 +0900

    deploy test

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /home/ubuntu/vting/server/dist
 
-# export DATABASE_URL=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_URL --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_URL=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_URL --query Parameters[0].Value | sed 's/"//g')
 export 
-PORT=$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value | sed 's/"//g')
+PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start index.js
