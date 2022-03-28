b1d91a372098fa723fb11f8f3bee19b416e149bf
elina <elinapark0818@gmail.com>
2022-03-28 23:40:29 +0900

    병합 해결

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -9,18 +9,7 @@ import { request } from "http";
 import { read } from "fs";
 const jwt = require("jsonwebtoken");
 
-<<<<<<< HEAD
-interface controller {
-  post: any;
-  get: any;
-}
-
-export let controller = {
-  post: async (req: Request, res: Response) => {
-    const { email } = req.body;
-=======
 require("dotenv").config();
->>>>>>> d4a5ad5481d9863327e1b9fc06e172b3da6b873f
 
 // const clientID = process.env.GITHUB_CLIENT_ID;
 // const clientSecret = process.env.GITHUB_CLIENT_SECRET;
@@ -232,14 +221,6 @@ export let UserController = {
       }
     },
   },
-<<<<<<< HEAD
-
-  get: async (req: Request, res: Response) => {
-    return res.status(200).json({ message: "Vting" });
-  },
-};
-=======
->>>>>>> d4a5ad5481d9863327e1b9fc06e172b3da6b873f
 
   userInfo: {
     get: async (req: Request, res: Response) => {
