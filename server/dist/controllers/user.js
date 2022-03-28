29e1a57cccfc389ece862b5dd2cd1a7b5e49b950
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2022-03-28 13:35:28 +0900

    Feat : vote

--- server/dist/controllers/user.js
+++ server/dist/controllers/user.js
@@ -12,7 +12,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserController = void 0;
 const __1 = require("..");
 const jwt = require("jsonwebtoken");
-
 require("dotenv").config();
 exports.UserController = {
     //회원가입과 탈퇴시 모두 사용가능한 체크
@@ -143,7 +142,6 @@ exports.UserController = {
                 yield __1.db.collection("user").deleteOne({ user_id: user_id });
                 // 쿠키에 토큰 삭제하기
                 yield res.clearCookie("accessToken", {
-
                     sameSite: "none",
                     secure: true,
                 });
@@ -219,8 +217,6 @@ exports.UserController = {
             catch (_b) {
                 return res.status(400).json({ message: "Bad request" });
             }
-
         }),
     },
-
 };
