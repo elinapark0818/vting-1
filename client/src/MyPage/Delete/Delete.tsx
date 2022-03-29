990f209bfca43ee312f7e236fcf55c83f9427a1b
elina <elinapark0818@gmail.com>
2022-03-29 15:49:11 +0900

    Feat: 마이페이지 회원탈퇴 조건부렌더링 및  모달기능 추가

--- client/src/MyPage/Delete/Delete.tsx
+++ client/src/MyPage/Delete/Delete.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setIsLogin, RootState } from "../../store/index";
+import { setIsLogin, RootState, setUserInfo } from "../../store/index";
 import { useNavigate } from "react-router-dom";
 import "./Delete.scss";
 
@@ -10,6 +10,27 @@ const serverURL: string = "http://localhost:8000";
 function Delete() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const userInfo = useSelector((state: RootState) => state.userInfo);
+
+  // useEffect(() => {
+  //   const getUserInfo = async () => {
+  //     try {
+  //       const res = await axios.get(serverURL + "/user/" + userInfo._id);
+  //       if (res.status === 200) {
+  //         setUserInfo({
+  //           _id: res.data.data._id,
+  //           nickname: res.data.data.nickname,
+  //           email: res.data.data.email,
+  //           image: res.data.data.image,
+  //         });
+  //       }
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   getUserInfo();
+  // }, []);
 
   const [modalState, setModalState] = useState<boolean>(false);
 
@@ -28,6 +49,7 @@ function Delete() {
       });
       if (res.status === 200) {
         console.log("회원탈퇴완료===", res.data.data);
+        alert("회원탈퇴가 완료되었습니다.");
         // ? 로그아웃처리
         dispatch(setIsLogin(false));
         navigate("/");
@@ -38,32 +60,50 @@ function Delete() {
   };
 
   return (
-    // <div>
-    //   {modalState && (
-    //     <div>
-    //       <button onClick={() => openModal()}>회원탈퇴</button>
-    //       <div>
-    //         <button onClick={() => closeModal()}>x</button>
-    //         <p>정말 탈퇴하시겠습니까?</p>
-    //       </div>
-    //     </div>
-    //   )}
-    // </div>
     <div className="delete_container">
       <header className="delete_header">
         <h1>회원탈퇴</h1>
       </header>
 
       <main className="delete_wrap">
-        <h1>프로필 : "{"프로필"}"</h1>
-        <h1>닉네임 : "{"닉네임"}"</h1>
-        <h1>이메일 : "{"이메일"}"</h1>
+        {/* <h1>프로필 : {getUserInfo.image}</h1> */}
+        <h1>_id 나와라{userInfo._id}</h1>
+        <h1>닉네임 : {userInfo.nickname}</h1>
+        <h1>이메일 : {userInfo.email}</h1>
       </main>
 
       <div className="delete_btnWrap">
         <h1>정말 탈퇴하시겠습니까?</h1>
-        <button className="delete_btn">탈퇴하기</button>
+        <button className="delete_btn" onClick={openModal}>
+          탈퇴하기
+        </button>
       </div>
+      {modalState ? (
+        <div className="deleteModal_container">
+          <div className="deleteModal_background">
+            <div className="deleteModal_modal">
+              <button className="closeBtn" onClick={closeModal}>
+                X
+              </button>
+              <h3>
+                회원정보가 삭제되면 복구되지 않습니다.
+                <br />
+                회원탈퇴를 진행하시겠습니까?
+              </h3>
+              <div className="btnWrap">
+                <button className="delete_ok" onClick={() => deleteUser()}>
+                  확인
+                </button>
+                <button className="delete_cancel" onClick={closeModal}>
+                  취소
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
