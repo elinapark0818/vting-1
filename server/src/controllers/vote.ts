b3fc70b3e6854262d93ca3933755eb6ed08c9f18
OverFlowBIN <87470206+OverFlowBIN@users.noreply.github.com>
2022-03-30 22:35:52 +0900

    Feat : vote

--- server/src/controllers/vote.ts
+++ server/src/controllers/vote.ts
@@ -8,9 +8,10 @@ import express, {
 import { request } from "http";
 import { read } from "fs";
 import { ObjectId } from "mongodb";
-const jwt = require("jsonwebtoken");
-
-// QRcode
+import jwt, { JwtPayload } from "jsonwebtoken";
+import dotenv from "dotenv";
+import { off } from "process";
+dotenv.config();
 
 interface VoteType1 {
   title: string;
@@ -23,16 +24,6 @@ interface VoteType1 {
   response: { idx: number; content: string }[];
 }
 
-// interface VoteType2 {
-//   title: string;
-//   format: string;
-//   manytimes: string;
-//   multiple?: string;
-//   type?: string[];
-//   items?: { idx: number; content: string; count: number }[];
-//   undergoing: true;
-// }
-
 export let VoteController = {
   test: {
     get: async (req: Request, res: Response) => {},
@@ -40,7 +31,6 @@ export let VoteController = {
 
   create: {
     post: async (req: Request & { body: any }, res: Response) => {
-      // req.body
       const {
         title,
         format,
@@ -54,147 +44,195 @@ export let VoteController = {
       // access code(6-digits) 만들기
       let randomNum: any = Math.random();
       let url = randomNum.toFixed(6) * 1000000;
-      //TODO: 마지막 자리가 0이면 다섯자리만 나옴.. 지금.. 아니면 6자리.
-
-      // user_id 가져오기 (from accessToken)
-      // const token = res.header;
-      // const accessToken: any = token.accessToken.split(" ")[1];
-      // const { user_id } = jwt.verify(accessToken, process.env.ACCESS_SECRET);
 
       try {
-        // format에 따라 vote 데이터 DB 저장하기
-        // FIXME: FORMAT 'bar'
-        if (format === "bar") {
-          let objectId: string;
-          db.collection("vote").insertOne(
-            {
-              // user_id,
-              url,
-              title,
-              format,
-              type,
-              items,
-              multiple,
-              manytimes,
-              undergoing: true,
-              create_at: new Date(),
-            },
-            async (err: Error, data: any) => {
-              // random url(6digit) 만들어 주기
-              objectId = await data.insertedId.toString();
+        // 헤더에 token 받아오기
+        let userId: string;
+        if (
+          req.headers.authorization &&
+          req.headers.authorization.split(" ")[0] === "Bearer"
+        ) {
+          let authorization: string | undefined = req.headers.authorization;
+          let token: string = authorization.split(" ")[1];
+          jwt.verify(
+            token,
+            process.env.ACCESS_SECRET as jwt.Secret,
+            async (err, data: any) => {
+              // user_id 변수 설정
+              userId = data.user_id;
 
-              let madeVote = await db
-                .collection("vote")
-                .findOne({ _id: new ObjectId(objectId) });
+              // format에 따라 vote 데이터 DB 저장하기
+              // FIXME: FORMAT 'bar'
+              if (format === "bar") {
+                let objectId: string;
 
-              // 응답 보내기
-              return res.status(201).json({
-                data: {
-                  _id: madeVote._id,
-                  title: madeVote.title,
-                  items: madeVote.items,
-                  url,
-                  create_at: madeVote.create_at,
-                },
-              });
+                await db.collection("vote").insertOne(
+                  {
+                    user_id: data.user_id,
+                    url,
+                    title,
+                    format,
+                    type,
+                    items,
+                    multiple,
+                    manytimes,
+                    undergoing: true,
+                    create_at: new Date(),
+                  },
+                  async (err: Error, data: any) => {
+                    // random url(6digit) 만들어 주기
+                    objectId = await data.insertedId.toString();
+
+                    // 유저 데이터에 vote 넣어주기
+                    await db
+                      .collection("user")
+                      .updateOne(
+                        { user_id: userId },
+                        { $push: { vote: new ObjectId(objectId) } }
+                      );
+
+                    // 만들어진 투표 => 골라서 응답 보내주기
+                    let madeVote = await db
+                      .collection("vote")
+                      .findOne({ _id: new ObjectId(objectId) });
+
+                    // 응답 보내기
+                    return res.status(201).json({
+                      data: {
+                        _id: madeVote._id,
+                        title: madeVote.title,
+                        items: madeVote.items,
+                        url,
+                        create_at: madeVote.create_at,
+                      },
+                    });
+                  }
+                );
+                // FIXME: FORMAT 'open ended'
+              } else if (format === "open ended") {
+                db.collection("vote").insertOne(
+                  {
+                    user_id: data.user_id,
+                    url,
+                    title,
+                    format,
+                    manytimes,
+                    response,
+                    undergoing: true,
+                    created_at: new Date(),
+                  },
+                  async (err: Error, data: any) => {
+                    // 방금 만든 objectId 보내주기
+                    let objectId: string = await data.insertedId.toString();
+                    let madeVote = await db
+                      .collection("vote")
+                      .findOne({ _id: new ObjectId(objectId) });
+
+                    // 유저 데이터에 vote 넣어주기
+                    await db
+                      .collection("user")
+                      .updateOne(
+                        { user_id: userId },
+                        { $push: { vote: new ObjectId(objectId) } }
+                      );
+
+                    // 응답 보내기
+                    return res.status(201).json({
+                      data: {
+                        _id: madeVote._id,
+                        title: madeVote.title,
+                        response: madeVote.response,
+                        create_at: madeVote.create_at,
+                        url,
+                      },
+                    });
+                  }
+                );
+                // FIXME: FORMAT 'vs'
+              } else if (format === "vs") {
+                db.collection("vote").insertOne(
+                  {
+                    user_id: data.user_id,
+                    url,
+                    title,
+                    format,
+                    manytimes,
+                    items,
+                    undergoing: true,
+                    created_at: new Date(),
+                  },
+                  async (err: Error, data: any) => {
+                    // 방금 만든 objectId 보내주기
+                    let objectId: string = await data.insertedId.toString();
+                    let madeVote = await db
+                      .collection("vote")
+                      .findOne({ _id: new ObjectId(objectId) });
+
+                    // 유저 데이터에 vote 넣어주기
+                    await db
+                      .collection("user")
+                      .updateOne(
+                        { user_id: userId },
+                        { $push: { vote: new ObjectId(objectId) } }
+                      );
+
+                    // 응답 보내기
+                    return res.status(201).json({
+                      data: {
+                        _id: madeVote._id,
+                        title: madeVote.title,
+                        items: madeVote.items,
+                        create_at: madeVote.create_at,
+                        url,
+                      },
+                    });
+                  }
+                );
+                // FIXME: FORMAT 'word cloud'
+              } else if (format === "word cloud") {
+                db.collection("vote").insertOne(
+                  {
+                    user_id: data.user_id,
+                    url,
+                    title,
+                    format,
+                    manytimes,
+                    items,
+                    undergoing: true,
+                    created_at: new Date(),
+                  },
+                  async (err: Error, data: any) => {
+                    // 방금 만든 objectId 보내주기
+                    let objectId: string = await data.insertedId.toString();
+                    let madeVote = await db
+                      .collection("vote")
+                      .findOne({ _id: new ObjectId(objectId) });
+
+                    // 유저 데이터에 vote 넣어주기
+                    await db
+                      .collection("user")
+                      .updateOne(
+                        { user_id: userId },
+                        { $push: { vote: new ObjectId(objectId) } }
+                      );
+
+                    // 응답 보내기
+                    return res.status(201).json({
+                      data: {
+                        _id: madeVote._id,
+                        title: madeVote.title,
+                        items: madeVote.items,
+                        create_at: madeVote.create_at,
+                        url,
+                      },
+                    });
+                  }
+                );
+              }
             }
           );
-          // FIXME: FORMAT 'open ended'
-        } else if (format === "open ended") {
-          db.collection("vote").insertOne(
-            {
-              // user_id,
-              url,
-              title,
-              format,
-              manytimes,
-              response,
-              undergoing: true,
-              created_at: new Date(),
-            },
-            async (err: Error, data: any) => {
-              // 방금 만든 objectId 보내주기
-              let objectId: string = await data.insertedId.toString();
-              let madeVote = await db
-                .collection("vote")
-                .findOne({ _id: new ObjectId(objectId) });
-
-              // 응답 보내기
-              return res.status(201).json({
-                data: {
-                  _id: madeVote._id,
-                  title: madeVote.title,
-                  response: madeVote.response,
-                  create_at: madeVote.create_at,
-                  url,
-                },
-              });
-            }
-          );
-          // FIXME: FORMAT 'vs'
-        } else if (format === "vs") {
-          db.collection("vote").insertOne(
-            {
-              // user_id,
-              url,
-              title,
-              format,
-              manytimes,
-              items,
-              undergoing: true,
-              created_at: new Date(),
-            },
-            async (err: Error, data: any) => {
-              // 방금 만든 objectId 보내주기
-              let objectId: string = await data.insertedId.toString();
-              let madeVote = await db
-                .collection("vote")
-                .findOne({ _id: new ObjectId(objectId) });
-
-              // 응답 보내기
-              return res.status(201).json({
-                data: {
-                  _id: madeVote._id,
-                  title: madeVote.title,
-                  items: madeVote.items,
-                  create_at: madeVote.create_at,
-                  url,
-                },
-              });
-            }
-          );
-          // FIXME: FORMAT 'word cloud'
-        } else if (format === "word cloud") {
-          db.collection("vote").insertOne(
-            {
-              // user_id,
-              url,
-              title,
-              format,
-              manytimes,
-              items,
-              undergoing: true,
-              created_at: new Date(),
-            },
-            async (err: Error, data: any) => {
-              // 방금 만든 objectId 보내주기
-              let objectId: string = await data.insertedId.toString();
-              let madeVote = await db
-                .collection("vote")
-                .findOne({ _id: new ObjectId(objectId) });
-
-              // 응답 보내기
-              return res.status(201).json({
-                data: {
-                  _id: madeVote._id,
-                  title: madeVote.title,
-                  items: madeVote.items,
-                  create_at: madeVote.create_at,
-                  url,
-                },
-              });
-            }
-          );
+        } else {
+          //TODO: 비회원 일때 생성 관련된 응답 만들기!
         }
       } catch {
         return res.status(400);
@@ -205,6 +243,7 @@ export let VoteController = {
   delete: {
     delete: async (req: Request & { params: any }, res: Response) => {
       const voteId = req.params;
+      //TODO: user data에 해당 vote(배열로 되어있음) 삭제해야됨
 
       db.collection("vote").deleteOne(
         { _id: new ObjectId(voteId) },
