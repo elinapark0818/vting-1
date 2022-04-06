760758a5573310bd908c423291d33cbb43cc498c
elina <elinapark0818@gmail.com>
2022-04-07 00:17:29 +0900

    Fix: Footer 위치 조정

--- client/src/Info/Footer.tsx
+++ client/src/Info/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { SiGithub } from "react-icons/si";
+// import { SiGithub } from "react-icons/si";
 import "./Footer.scss";
+import Logo from "../assets/yof_logo-17.jpg";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 function Footer() {
   return (
@@ -8,51 +10,77 @@ function Footer() {
       <main className="footer_wrap">
         <div className="footer_team">
           <div className="footer_yof">
-            <p>김영빈</p>
-            <p>팀장</p>
-            <p>Full-Stack</p>
-            <div className="github">
-              <SiGithub
-                onClick={() =>
-                  window.open("https://github.com/overflowbin", "_blank")
-                }
-              />
-            </div>
+            <img
+              src={Logo}
+              alt="YOf"
+              style={{ width: "6em", borderRadius: "50%", marginRight: "1em" }}
+            />
           </div>
           <div className="footer_yof">
-            <p>박성경</p>
-            <p>팀장</p>
-            <p>Full-Stack</p>
-            <div className="github">
-              <SiGithub
-                onClick={() =>
-                  window.open("https://github.com/biblepark", "_blank")
-                }
-              />
+            <div>TEAM : YOF</div>
+            <p>Project : Vting</p>
+            <p>Period : 22.03.14~22.04.08</p>
+            <div
+              className="github"
+              onClick={() =>
+                window.open("https://github.com/codestates/vting", "_blank")
+              }
+            >
+              Go to github <FaExternalLinkAlt style={{ width: "0.7em" }} />
             </div>
           </div>
+
           <div className="footer_yof">
-            <p>이초록</p>
-            <p>팀원</p>
+            <p>김영빈(팀장)</p>
+            <p>Full-Stack</p>
+            <p>bin11788@gmail.com</p>
+            <div
+              className="github"
+              onClick={() =>
+                window.open("https://github.com/overflowbin", "_blank")
+              }
+            >
+              OverFlowBIN <FaExternalLinkAlt style={{ width: "0.7em" }} />
+            </div>
+          </div>
+
+          <div className="footer_yof">
+            <p>박성경(팀원)</p>
+            <p>Full-Stack</p>
+            <p>stzard@gmail.com</p>
+            <div
+              className="github"
+              onClick={() =>
+                window.open("https://github.com/biblepark", "_blank")
+              }
+            >
+              Bible Park <FaExternalLinkAlt style={{ width: "0.7em" }} />
+            </div>
+          </div>
+
+          <div className="footer_yof">
+            <p>이초록(팀원)</p>
             <p>Back-End</p>
-            <div className="github">
-              <SiGithub
-                onClick={() =>
-                  window.open("https://github.com/2cho6", "_blank")
-                }
-              />
+            <p>evenabee@gmail.com</p>
+            <div
+              className="github"
+              onClick={() => window.open("https://github.com/2cho6", "_blank")}
+            >
+              2cho6 <FaExternalLinkAlt style={{ width: "0.7em" }} />
             </div>
           </div>
+
           <div className="footer_yof">
-            <p>박윤정</p>
-            <p>팀원</p>
-            <p>Front_End</p>
-            <div className="github">
-              <SiGithub
-                onClick={() =>
-                  window.open("https://github.com/elinapark0818", "_blank")
-                }
-              />
+            <p>박윤정(팀원)</p>
+            <p>Front-End</p>
+            <p>elinapark0818@gmail.com</p>
+            <div
+              className="github"
+              onClick={() =>
+                window.open("https://github.com/elinapark0818", "_blank")
+              }
+            >
+              Elina Park <FaExternalLinkAlt style={{ width: "0.7em" }} />
             </div>
           </div>
         </div>
