e987f0bebde02ae371697aa4cb473d1232267cfc
elina <elinapark0818@gmail.com>
2022-04-07 00:18:39 +0900

    Fix: 불필요한 로그 삭제

--- client/src/MyPage/Edit/Edit.tsx
+++ client/src/MyPage/Edit/Edit.tsx
@@ -15,7 +15,6 @@ function Edit() {
   const dispatch = useDispatch();
   const userInfo = useSelector((state: RootState) => state.userInfo);
 
-  //* 닉네임 프로필 변경
   const [patchUserInfo, setPatchUserInfo] = useState<PatchUser>({
     name: "",
     password: "",
@@ -31,20 +30,14 @@ function Edit() {
     setPatchUserInfo({ ...patchUserInfo, [name]: value });
   };
 
-  // * 닉네임, 비밀번호 변경
-  // ! Cannot PATCH => `${serverURL}/user` 요거로 바뀐거임!
-  // ! 심각한 오류 => 비밀번호를 비워두고 닉네임만 바꾸면 비번을 찾을 수가 없음..
-  // todo: 방안 1. 닉네임 변경 / 비밀번호 변경 분기하기
   const EditUserInfo = async () => {
     let accessToken = localStorage.getItem("accessToken");
     let sendBody;
-    // * 패스워드에 입력된 값이 있다면 바디에 내용을 담아서 보낸다.(변경할 nickname, password)
     if (patchUserInfo.password !== "")
       sendBody = {
         nickname: patchUserInfo.name || userInfo.nickname,
         password: patchUserInfo.password,
       };
-    // * 패스워드에 입력된 값이 없다면 바디에 변경할 nickname만 담아서 보낸다
     else
       sendBody = {
         nickname: patchUserInfo.name || userInfo.nickname,
@@ -66,8 +59,6 @@ function Edit() {
               })
             );
             alert("회원정보가 수정되었습니다.");
-          } else {
-            console.log("Bad Request 입니다. 400에러");
           }
         });
     } catch (err) {
@@ -84,8 +75,6 @@ function Edit() {
       <main className="edit_wrap">
         <div className="edit_userProfile">
           <div className="edit_profile">
-            {/* <h3>프로필</h3> */}
-
             <label htmlFor="file" className="edit_btn">
               업로드
             </label>
@@ -106,7 +95,6 @@ function Edit() {
               name="name"
               onChange={edit_onChangeName}
               type="text"
-              // value={userInfo.nickname}
               placeholder={userInfo.nickname}
             />
           </div>
