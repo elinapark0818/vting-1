c6785d1d0e27d945618695615bbac2f425ed1e2d
biblePark <stzard@gmail.com>
2022-04-07 15:38:11 +0900

    Fix : NavBar 에러 수정, Path : '/client'

--- client/src/Navbar/Navbar.tsx
+++ client/src/Navbar/Navbar.tsx
@@ -27,33 +27,6 @@ function Navbar() {
   }, [location]);
 
   const NavbarUserInfo = async () => {
-    let accessToken = localStorage.getItem("accessToken");
-    try {
-      await axios
-        .get(`${serverURL}/auth`, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-            withCredentials: true,
-          },
-        })
-        .then((res) => {
-          if (res.status === 200) {
-            settingLogin();
-            dispatch(
-              setUserInfo({
-                _id: res.data.data._id,
-                nickname: res.data.data.nickname,
-                email: res.data.data.user_id,
-                image: res.data.data.image,
-              })
-            );
-          } else {
-            console.error("400 Error");
-          }
-        });
-    } catch (err) {
-      console.log(err);
-
     if (document.location.href.includes("vote")) {
       // vote. 경로로 접속한 경우이므로 로그인 요청을 보내지 않습니다.
     } else {
@@ -83,7 +56,6 @@ function Navbar() {
       } catch (err) {
         console.log(err);
       }
-
     }
   };
 
@@ -158,7 +130,6 @@ function Navbar() {
                     >
                       SingOut
                     </div>
-
                   </div>
                 </ul>
               </div>
@@ -208,5 +179,4 @@ function VotingBanner() {
     </div>
   );
 }
-
 export default Navbar;
